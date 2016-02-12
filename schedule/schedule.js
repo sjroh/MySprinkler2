a672a8dfa3d8a2d6c6e05f95b079e1ff75fbbdcf
kbwahl <kbwahl@tamu.edu>
2016-02-11 23:03:54 -0600

    google sign in attempt1

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -2,6 +2,21 @@
  * Created by Kyle on 2/10/2016.
  */
 $(document).ready(function(){
+
+    // initialize input widgets first
+    $('#datepairExample .time').timepicker({
+        'showDuration': true,
+        'timeFormat': 'g:ia'
+    });
+
+    $('#datepairExample .date').datepicker({
+        'format': 'yyyy-m-d',
+        'autoclose': true
+    });
+
+    // initialize datepair
+    $('#datepairExample').datepair();
+
     function checkWidth(){
         if($(window).width() < 600){
             $(".mobile").show();
@@ -22,6 +37,13 @@ $(document).ready(function(){
             $(".menu-btn").removeClass("glyphicon-chevron-left");
             $(".menu-btn").addClass("glyphicon-chevron-right");
         }
-    })
+    });
 
+    $(".addEvent").click(function(){
+        //if valid date, then
+            //code here to save water event to google calendar & google drive & raspberry pi
+        //else
+            //error message
+        alert("You have added a water event!");
+    });
 });
