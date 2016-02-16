1c0266e1b39708641729650bec8336a3759a3128
kbwahl <kbwahl@tamu.edu>
2016-02-15 19:03:17 -0600

    sign in changes

--- home/home.js
+++ home/home.js
@@ -1,14 +1,18 @@
 /**
  * Created by Kyle on 2/8/2016.
  */
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail());
-    window.location = "http://sjroh.github.io/MySprinkler2/home/home.html";
-
 }
 
 $(document).ready(function(){
--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -11,6 +11,12 @@ function onSignIn(googleUser) {
     window.location = "http://sjroh.github.io/MySprinkler2/home/home.html";
 
 }
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
 
 //jQuery to collapse the navbar on scroll
 $(document).ready(function(){
