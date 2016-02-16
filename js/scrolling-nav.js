4347d166c9295feb2dbcee1c87d641e09eb1854b
kbwahl <kbwahl@tamu.edu>
2016-02-15 19:15:46 -0600

    test

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -12,6 +12,7 @@ function onSignIn(googleUser) {
 
 }
 function signOut() {
+    alert("testing");
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
