a1eb358670fa5f0b7d051db03d1874957c11edb3
kbwahl <kbwahl@tamu.edu>
2016-02-17 15:37:48 -0600

    more date stuff

--- js/weather.js
+++ js/weather.js
@@ -5,16 +5,11 @@ var weatherData = {};
 $.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat=30.627977&lon=-96.334407&cnt=7&mode=json&appid=0039a67282bf9ff15995e2c340d6906b', function(data){
     weatherData = data.list;
    // console.log(weatherData);
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     for(var i = 0; i < weatherData.length; i++){
         var idName = "#day" + (i+1).toString();
         var date = new Date(weatherData[i].dt);
-        $(idName).text(date.getMonth() + " " + date.getDay());
+        $(idName).text(months[date.getMonth()] + " " + date.getDay());
+        console.log("Date: " + [date.getMonth()] + " " + date.getDay());
     }
-
-
-
-    var d = new Date(weatherData[0].dt);
-    var day = d.getDay();
-    var month = d.getMonth();
-    console.log(d);
 });
