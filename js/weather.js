16addd8ffd5e15a547755e328b58a5fa6e82782f
kbwahl <kbwahl@tamu.edu>
2016-02-17 15:06:45 -0600

    api works, testing date

--- js/weather.js
+++ js/weather.js
@@ -2,7 +2,9 @@
  * Created by Kyle on 2/17/2016.
  */
 var weatherData = {};
-$.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat=30.627977&lon=-96.334407&cnt=10&mode=json&appid=0039a67282bf9ff15995e2c340d6906b', function(data){
+$.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat=30.627977&lon=-96.334407&cnt=7&mode=json&appid=0039a67282bf9ff15995e2c340d6906b', function(data){
     weatherData = data;
-    console.log(weatherData);
+   // console.log(weatherData);
+    var d = new Date(weatherData.list[0].dt);
+    console.log(d);
 });
