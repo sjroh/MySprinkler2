48c31e067e8c6d070debdf5c1f0bae664a3b7b92
kbwahl <kbwahl@tamu.edu>
2016-02-17 11:15:54 -0600

    fix

--- js/weather.js
+++ js/weather.js
@@ -2,7 +2,7 @@
  * Created by Kyle on 2/17/2016.
  */
 var weatherData = {};
-$.getJson('api.openweathermap.org/data/2.5/forecast/daily?lat={30.627977}&lon={-96.334407}&cnt={7}', function(data){
+$.getJSON('api.openweathermap.org/data/2.5/forecast/daily?lat={30.627977}&lon={-96.334407}&cnt={7}', function(data){
     weatherData = data;
     console.log(weatherData);
 });
