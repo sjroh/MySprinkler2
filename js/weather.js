ddc685f6799e9d35863892f096cccda6662843ac
kbwahl <kbwahl@tamu.edu>
2016-02-18 13:22:07 -0600

    weather table change

--- js/weather.js
+++ js/weather.js
@@ -6,6 +6,7 @@ $.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat=30.627977&lon=-
     weatherData = data.list;
    // console.log(weatherData);
     var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
     for(var i = 0; i < weatherData.length; i++){
         var idName = "#day" + (i+1).toString();
         var idWName = "#w" + (i+1).toString();
@@ -13,7 +14,7 @@ $.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat=30.627977&lon=-
         date.setUTCSeconds(weatherData[i].dt);
        // date.setTime(weatherData[i].dt * 1000);//date = epoch value * 1000
         //date.setUTCSeconds()
-        $(idName).text(months[date.getMonth()] + "  " + date.getUTCDate());
+        $(idName).html(days[date.getDay()] + "<br>" + months[date.getMonth()] + "  " + date.getUTCDate());
         console.log(weatherData[i].dt + ": " + date.toDateString());
         $(idWName).html("<img src='http://openweathermap.org/img/w/" + weatherData[i].weather[0].icon + ".png'>");
     }
