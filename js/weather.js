b4142910122a5f9904a1ba2e9eb6b44c6aab8da4
kbwahl <kbwahl@tamu.edu>
2016-02-18 13:03:51 -0600

    weather icons

--- js/weather.js
+++ js/weather.js
@@ -8,11 +8,13 @@ $.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat=30.627977&lon=-
     var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     for(var i = 0; i < weatherData.length; i++){
         var idName = "#day" + (i+1).toString();
+        var idWName = "#w" + (i+1).toString();
         var date = new Date(0);
         date.setUTCSeconds(weatherData[i].dt);
        // date.setTime(weatherData[i].dt * 1000);//date = epoch value * 1000
         //date.setUTCSeconds()
-        $(idName).text(months[date.getMonth()] + " " + date.getUTCDate());
+        $(idName).text(months[date.getMonth()] + "  " + date.getUTCDate());
         console.log(weatherData[i].dt + ": " + date.toDateString());
+        $(idWName).html("<img src='http://openweathermap.org/img/w/" + weatherData[i].weather[0].icon + ".png'>");
     }
 });
