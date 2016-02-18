062be2a31a0c44a095315dd3fa1824e67c883c3a
exiledwolfking <kbwahl@neo.tamu.edu>
2016-02-18 12:22:39 -0600

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -8,7 +8,7 @@ $.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat=30.627977&lon=-
     var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     for(var i = 0; i < weatherData.length; i++){
         var idName = "#day" + (i+1).toString();
-        var date = new Date(weatherData[i].dt*1000);
+        var date = new Date(weatherData[i].dt);
        // date.setTime(weatherData[i].dt * 1000);//date = epoch value * 1000
         //date.setUTCSeconds();
         $(idName).text(months[date.getMonth()] + " " + date.getDay());
