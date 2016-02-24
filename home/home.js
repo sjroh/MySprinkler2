4bc314e3749d6a78eaff857dd316d4c8926c8be4
kbwahl <kbwahl@tamu.edu>
2016-02-24 15:15:07 -0600

    gdrive file search test

--- home/home.js
+++ home/home.js
@@ -22,6 +22,7 @@ function onSuccess(googleUser) {
     signedIn = true;
     $("#userName").html(googleUser.getBasicProfile().getName());
     checkStatus();
+    listFilesInApplicationDataFolder(checkForDriveFile);
 }
 function onFailure(error) {
     console.log(error);
@@ -58,8 +59,37 @@ function checkStatus(){
     }
 }
 
+function checkForDriveFile(result) {
+    console.log(result);
+    alert("result given");
+}
 
 
+/**
+ * List all files contained in the Application Data folder.
+ *
+ * @param {Function} callback Function to call when the request is complete.
+ */
+function listFilesInApplicationDataFolder(callback) {
+    var retrievePageOfFiles = function(request, result) {
+        request.execute(function(resp) {
+            result = result.concat(resp.items);
+            var nextPageToken = resp.nextPageToken;
+            if (nextPageToken) {
+                request = gapi.client.drive.files.list({
+                    'pageToken': nextPageToken
+                });
+                retrievePageOfFiles(request, result);
+            } else {
+                callback(result);
+            }
+        });
+    }
+    var initialRequest = gapi.client.drive.files.list({
+        'q': 'title = \'settings\''
+    });
+    retrievePageOfFiles(initialRequest, []);
+}
 
 $(document).ready(function(){
     function checkWidth(){
