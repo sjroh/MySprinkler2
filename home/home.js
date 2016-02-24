d9925d57a42262844d23d038c863738642cde0dc
kbwahl <kbwahl@tamu.edu>
2016-02-24 13:37:30 -0600

    sign in changes

--- home/home.js
+++ home/home.js
@@ -5,6 +5,17 @@ var signedIn = false;
 
 var globalVariables = {};
 
+var options = new gapi.auth2.SigninOptionsBuilder(
+    {'scope': 'email https://www.googleapis.com/auth/drive'});
+
+googleUser = auth2.currentUser.get();
+googleUser.grant(options).then(
+    function(success){
+        console.log(JSON.stringify({message: "success", value: success}));
+    },
+    function(fail){
+        alert(JSON.stringify({message: "fail", value: fail}));
+    });
 /*function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
