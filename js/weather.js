3cbcf77255845ad8c80d3fe426abf34732d7e725
kbwahl <kbwahl@tamu.edu>
2016-02-27 14:34:15 -0600

    gdrive stuff

--- js/weather.js
+++ js/weather.js
@@ -138,7 +138,8 @@ function insertFileInApplicationDataFolder(jData, fileName) {
     var contentType = 'application/json';
     var metadata = {
         'title': fileName,
-        'mimeType': contentType
+        'mimeType': contentType,
+        'parents': [{'id': 'appfolder'}]
     };
     var base64Data = btoa(JSON.stringify(jData));
     var multipartRequestBody =
