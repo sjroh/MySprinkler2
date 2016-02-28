4c03bfc99b1d4f390949c8dda52ccce281f07cbd
kbwahl <kbwahl@tamu.edu>
2016-02-28 17:25:35 -0600

    d

--- home/home.js
+++ home/home.js
@@ -112,8 +112,8 @@ function listFilesInApplicationDataFolder() {
 
 function downloadFile(file) {
     if (file.downloadUrl) {
-        console.log(gapi.auth2);
-        var accessToken = gapi.auth2.getToken().access_token;
+        console.log(gapi.auth);
+        var accessToken = gapi.auth.getToken().access_token;
         var xhr = new XMLHttpRequest();
         xhr.open('GET', file.downloadUrl);
         xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
