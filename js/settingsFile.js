29ff90b5a0deee919e063b6bb2eb9bceafcb16cd
kbwahl <kbwahl@tamu.edu>
2016-02-29 17:44:56 -0600

    s

--- js/settingsFile.js
+++ js/settingsFile.js
@@ -52,20 +52,20 @@ function setLocation(position) {
     //should store in google app data now
     var getString = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + weatherData.lat + "&lon=" + weatherData.long +  "&cnt=7&mode=json&appid=0039a67282bf9ff15995e2c340d6906b";
     $.get(getString, function(data){
-        weatherData = data.list;
+        weatherData.list = data.list;
         // console.log(weatherData);
         var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
         var days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
-        for(var i = 0; i < weatherData.length; i++){
+        for(var i = 0; i < weatherData.list.length; i++){
             var idName = "#day" + (i+1).toString();
             var idWName = "#w" + (i+1).toString();
             var date = new Date(0);
-            date.setUTCSeconds(weatherData[i].dt);
+            date.setUTCSeconds(weatherData.list[i].dt);
             // date.setTime(weatherData[i].dt * 1000);//date = epoch value * 1000
             //date.setUTCSeconds()
             $(idName).html(days[date.getDay()] + "<br>" + months[date.getMonth()] + "  " + date.getUTCDate());
-            console.log(weatherData[i].dt + ": " + date.toDateString());
-            $(idWName).html("<img src='http://openweathermap.org/img/w/" + weatherData[i].weather[0].icon + ".png'>");
+            console.log(weatherData.list[i].dt + ": " + date.toDateString());
+            $(idWName).html("<img src='http://openweathermap.org/img/w/" + weatherData.list[i].weather[0].icon + ".png'>");
         }
     });
 
