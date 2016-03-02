20d2167fdf9d2f712a349f6e209671186da4295a
kbwahl <kbwahl@tamu.edu>
2016-03-01 22:53:36 -0600

    a

--- home/home.js
+++ home/home.js
@@ -119,6 +119,10 @@ function downloadFile(file) {
         xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
         xhr.onload = function() {
             console.log(xhr.responseText);
+            var jsonResponse = updateJson(xhr.responseText);//update it (remove old entries older than 7 days// make new schedule, push to google drive & check weather here?
+            //NOTE: should probably put some time stamp in the events.txt file to store when schedule/weather was updated & checked
+            //perhaps we could check up to twice a day (if user logs in at least twice a day).
+            addEventsToOverview(jsonResponse);//add events to overview on home page
         };
         xhr.onerror = function() {
             console.log("ERROR");
@@ -126,6 +130,89 @@ function downloadFile(file) {
         xhr.send();
     } else {
         console.log("No file.downloadUrl");
+    }
+}
+
+function updateJson(jsonRetrieved){
+
+}
+
+function addEventsToOverview(jsonResponse){
+    //sample json object
+    jsonResponse = {
+        prev: {},
+        current: [
+            {
+                sTime: 1456958100,
+                eTime: 1456958400,
+                zoneID: 1
+            },
+            {
+                sTime: 1456950000,
+                eTime: 1456951200,
+                zoneID: 1
+            },
+            {
+                sTime: 1457210400,
+                eTime: 1457210760,
+                zoneID: 2
+            },
+            {
+                sTime: 1457297400,
+                eTime: 1457297700,
+                zoneID: 3
+            },
+            {
+                sTime: 1457268900,
+                eTime: 1457269500,
+                zoneID: 4
+            }
+        ]
+    };
+
+    var currWeekEpoch = [];
+    var date = Math.round(new Date().getTime()/1000.0);
+    currWeekEpoch.push(date);
+    for(var i = 0; i < 6; i++){
+        var newDate = currWeekEpoch[i] + 86400;
+        currWeekEpoch.push(newDate);
+    }
+
+    var sortedByDay = new Array(7);
+    for(i = 0; i < 7; i++){
+        sortedByDay[i] = [];
+    }
+    for(i = 0; i < jsonResponse.current.length; i++){
+        var startTime = jsonResponse.current[i].sTime;
+        var endTime = jsonResponse.current[i].eTime;
+        var index = findIndexOfDay(startTime, currWeekEpoch);
+        if(sortedByDay[index].length == 0){
+            sortedByDay[index].push(jsonResponse.current[i]);
+        } else if(sortedByDay[index].length == 1){
+            if(sortedByDay[index][0].sTime > startTime)
+                sortedByDay[index].unshift(jsonResponse.current[i]);
+            else
+                sortedByDay[index].push(jsonResponse.current[i]);
+        } else {
+            for(var j = 0; j < sortedByDay[index].length; j++){
+                //add in jsonresponse.current[i] in right spot
+            }
+        }
+    }
+
+    //now iterate thru sortedByDay and add to home.html in proper table td
+
+}
+
+function findIndexOfDay(time, currWeekEpoch){
+    var date = new Date(0);
+    date.setUTCSeconds(time);
+
+    for(var i = 0; i < 7; i++){
+        var weekDate = new Date(0);
+        weekDate.setUTCSeconds(currWeekEpoch[i]);
+        if(date.getDay() === weekDate.getDay())
+            return i;
     }
 }
 
