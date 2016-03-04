d51d1d8feef97be4aece169738faf7df7c5f7813
arod7 <arod7@tamu.edu>
2016-03-03 18:43:36 -0600

    added sign out btn & weather

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -69,5 +69,14 @@ $(document).ready(function(){
         }
     });
 
-
 });
+
+	function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+        signedIn = false;
+        checkStatus();
+        window.location = "http://sjroh.github.io/MySprinkler2/";
+    });
+}
--- weather/weather.js
+++ weather/weather.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
             $(".menu-btn").addClass("glyphicon glyphicon-chevron-left");
         }
     }
+	
     checkWidth();
     $("#menu-toggle").click(function(){
         if($(".menu-btn").hasClass("glyphicon-chevron-right")){
@@ -24,6 +25,35 @@ $(document).ready(function(){
             $(".menu-btn").addClass("glyphicon-chevron-right");
         }
     });
+	
 
 
+});
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+        signedIn = false;
+        checkStatus();
+        window.location = "http://sjroh.github.io/MySprinkler2/";
+    });
+}
+
+$.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat=30.627977&lon=-96.334407&cnt=7&mode=json&appid=0039a67282bf9ff15995e2c340d6906b', function(data){
+    weatherData = data.list;
+    // console.log(weatherData);
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+    for(var i = 0; i < weatherData.length; i++){
+        var idName = "#day" + (i+1).toString();
+        var idWName = "#w" + (i+1).toString();
+        var date = new Date(0);
+        date.setUTCSeconds(weatherData[i].dt);
+        // date.setTime(weatherData[i].dt * 1000);//date = epoch value * 1000
+        //date.setUTCSeconds()
+        $(idName).html(days[date.getDay()] + "<br>" + months[date.getMonth()] + "  " + date.getUTCDate());
+        console.log(weatherData[i].dt + ": " + date.toDateString());
+        $(idWName).html("<img src='http://openweathermap.org/img/w/" + weatherData[i].weather[0].icon + ".png'>");
+    }
 });
