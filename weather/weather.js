3101c725122ab4f5a2d7e1b432e4a72793d273f2
kbwahl <kbwahl@tamu.edu>
2016-03-17 11:21:02 -0500

    fix again

--- weather/weather.js
+++ weather/weather.js
@@ -110,13 +110,3 @@ $(document).ready(function(){
 
 
 });
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-        signedIn = false;
-        checkStatus();
-        window.location = "http://sjroh.github.io/MySprinkler2/";
-    });
-}
