16b950e5e657bce18948d8d20f64b08c480d6af5
kbwahl <kbwahl@tamu.edu>
2016-03-20 12:50:58 -0500

    storm'

--- weather/weather.js
+++ weather/weather.js
@@ -87,12 +87,12 @@ $(document).ready(function(){
     function checkWidth(){
         if($(window).width() < 600){
             $(".mobile").show();
-            $(".slideshow").css('background-image', 'url(../images/rainmobile.jpg');
+            $(".slideshow").css('background-image', 'url(../images/stormmobile.jpg');
             $(".desktop").hide();
             $(".menu-btn").addClass("glyphicon glyphicon-chevron-right");
         } else{
             $(".mobile").hide();
-            $(".slideshow").css('background-image', 'url(../images/rainsmall.jpg');
+            $(".slideshow").css('background-image', 'url(../images/stormsmall.jpg');
             $(".desktop").show();
             $(".menu-btn").addClass("glyphicon glyphicon-chevron-left");
         }
