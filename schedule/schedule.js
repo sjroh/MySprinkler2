29a4fdfa9edc4a5aece91edc9fc9861bb699cfcb
kbwahl <kbwahl@tamu.edu>
2016-04-06 16:48:47 -0500

    fix?

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -134,7 +134,7 @@ $(document).ready(function(){
         var currDate = new Date();
         var sDate = datepair.getStartTime();
         var eDate = datepair.getEndTime();
-        if(currDate >= sDate || sDate == eDate)
+        if(currDate >= sDate || sDate >= eDate)
             $("#invalidTime").show();
         else
             $("#invalidTime").hide();
@@ -144,7 +144,7 @@ $(document).ready(function(){
         else
             $("#invalidZones").hide();
 
-        if(currDate < sDate && zonesClicked.length != 0 && sDate != eDate){
+        if(currDate < sDate && zonesClicked.length != 0 && sDate != eDate && sDate < eDate){
             $("#addModal").modal('hide');
             var event = {
                 sTime: Math.round(sDate.getTime()/1000.0),
