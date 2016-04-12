07c6725903b23a11f4e26e99e17b10c951effc20
kbwahl <kbwahl@tamu.edu>
2016-04-11 22:13:16 -0500

    fix

--- weather/weather.js
+++ weather/weather.js
@@ -85,10 +85,12 @@ $(document).ready(function(){
     if(!localStorage.getItem("settings")){
         $("#setupInstructions").show();
         setWeatherImage({main: null});
+        $("today").hide();
         console.log("couldn't retrieve settings obj from local storage");
     } else if(!localStorage.getItem("events")){
         $("#serverInstructions").show();
         setWeatherImage({main: null});
+        $("#today").hide();
         console.log("couldn't retrieve events obj from local storage");
     } else{
         $("#tableHolder").show();
